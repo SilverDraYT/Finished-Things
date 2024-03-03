ac1e2383cfc80b77f8c2f4e580edbce1d882593d
Cross <joselidue@gmail.com>
2024-03-03 17:17:11 +0100

    Fix

--- Works/Numguesser.py
+++ Works/Numguesser.py
@@ -1,23 +1,37 @@
 import random
 while True:
+   
+
+
+
     num=random.randint(1,10)
     #If there is not int() guess is a string and then it doesn't work   
-    guess=int(input('Elige un numero del 1 al 10:'))
+    try: 
+        while True:
+            guess=int(input('Elige un numero del 1 al 10:'))
    
-     #A lot of code stopped by a simple or, i love that      
-    if guess < 1 or guess > 10:
-        print('Pick a number between 1 and 10')
-    else:
-        if guess==num:
-            print('Well done')
-            break
-        else:
-            #I make it write the answer?
-            print('Try again')
-           
+     #A lot of code without writting because of a simple 'or', i love that      
+            if guess < 1 or guess > 10:
+                print('Pick a number between 1 and 10')
+            else:
+                if guess==num:
+                    print('Well done')
+                    break
+                else:
+                    if guess < num:
+                        print('Too low')
+                    else:
+                        print('Too high')    
+            
+                    print('Try again')
+    except:
+        print('Pick a number please')       
+    
+    print('The end')
+    break
 
-print('The end')
+
     
     
     
-#The enter gives error, fix that
+#The enter gives error, fix that(Fixed)
