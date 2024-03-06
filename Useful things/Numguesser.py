99f2f0462c7ac4157a092d761724488c95814b52
Cross <joselidue@gmail.com>
2024-03-06 17:46:01 +0100

    Update
    Idk if im going to start putting numbers to the updates

--- Useful things/Numguesser.py
+++ Useful things/Numguesser.py
@@ -1,33 +1,37 @@
 import random
 while True:
-    num=random.randint(1,10)
-    #If there is not int() guess is a string and then it doesn't work   
-    try: 
-        while True:
-            guess=int(input('Elige un numero del 1 al 10:'))
-   
-     #A lot of code without writting because of a simple 'or', i love that      
-            if guess < 1 or guess > 10:
-                print('Pick a number between 1 and 10')
-            else:
-                if guess==num:
-                    print('Well done')
-                    break
+        num=random.randint(1,10)
+        try: 
+            while True:
+                guess=int(input('Elige un numero del 1 al 10:'))
+                if guess < 1 or guess > 10:
+                    print('Pick a number between 1 and 10')
                 else:
-                    if guess < num:
-                        print('Too low')
+                    if guess==num:
+                        print('Well done')
+                        break
                     else:
-                        print('Too high')    
+                        if guess < num:
+                            print('Too low')
+                        else:
+                            print('Too high')    
             
-                    print('Try again')
-    except:
-        print('Pick a number please')       
-    
-    print('The end')
-    break
+                        print('Try again')
+        except:
+            print('Pick a number please')
+            continue 
+            
 
+        print('The End')
+        break
+
+            
+    
+       
+        
 
     
     
-    
+#If there is not int() guess is a string and then it doesn't work     
 #The enter gives error, fix that(Fixed)
+#A lot of code without writting because of a simple 'or', i love that   
